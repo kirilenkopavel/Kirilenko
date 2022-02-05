112a94b7a186822e38523fae88b7fa74b2c08c78
PawelKir <kirilenkopavel@bk.ru>
2022-02-06 00:14:40 +0400

    HomeWork_3

---
+++ src/main/java/org/example/TestKeys1.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Java Web UI
+ *@author Pavel Kirilenko
+ *@version 05.02.2022
+ */
+public class TestKeys1 {
+
+    public static void main( String[] args ) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments ("start-maximized");
+        options.addArguments ("--incognito");
+        options.addArguments ("disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.xpath("//div[@id=\"shopping_cart_container\"]/a")).click();
+        driver.findElement(By.id("continue-shopping")).click();
+        driver.findElement(By.id("remove-sauce-labs-backpack")).click();
+
+       driver.quit();
+
+
+
+    }
+}
---
+++ src/main/java/org/example/TestKeys4.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Java Web UI
+ *@author Pavel Kirilenko
+ *@version 05.02.2022
+ */
+public class TestKeys4 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com/");
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.xpath("//div[@id=\"shopping_cart_container\"]/a")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Иван");
+        driver.findElement(By.id("last-name")).sendKeys("Иванов");
+        driver.findElement(By.id("postal-code")).sendKeys("1111");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("finish")).click();
+
+        driver.quit();
+    }
+}
